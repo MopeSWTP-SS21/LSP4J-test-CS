f4c2473f484de96608841be7530f5334cf585548
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2021-05-28 14:11:04 +0200

    creates socket to get input/output steam

--- src/main/java/de/thm/mni/swtp/cs/lsp4jtest/LSP4jServer.java
+++ src/main/java/de/thm/mni/swtp/cs/lsp4jtest/LSP4jServer.java
@@ -6,6 +6,9 @@ import org.eclipse.lsp4j.jsonrpc.Launcher;
 import org.eclipse.lsp4j.launch.LSPLauncher;
 import org.eclipse.lsp4j.services.*;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.CompletableFuture;
 
 public class LSP4jServer implements LanguageServer {
@@ -34,9 +37,11 @@ public class LSP4jServer implements LanguageServer {
         return null;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         LSP4jServer server = new LSP4jServer();
-        Launcher<LanguageClient> launcher = LSPLauncher.createServerLauncher(server, inputStream, outputStream);
+        ServerSocket socket = new ServerSocket(667);
+        Socket connection = socket.accept();
+        Launcher<LanguageClient> launcher = LSPLauncher.createServerLauncher(server, connection.getInputStream(), connection.getOutputStream());
         LanguageClient client = launcher.getRemoteProxy();
         ((LanguageClientAware) server).connect(client);
         launcher.startListening();
