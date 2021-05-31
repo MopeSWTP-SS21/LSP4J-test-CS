5dc925e4bd7dbf4335d76b2a4675a1d981a2b088
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2021-05-31 16:09:42 +0200

    adds NonExplodingSocketInputStream

--- src/main/java/de/thm/mni/swtp/cs/lsp4jtest/SocketCloseTest.java
+++ src/main/java/de/thm/mni/swtp/cs/lsp4jtest/SocketCloseTest.java
@@ -6,6 +6,25 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketCloseTest {
+    private static class NonExplodingSocketInputStream extends InputStream {
+
+        private Socket socket;
+
+        public NonExplodingSocketInputStream(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public int read() throws IOException {
+            if (socket.isClosed()) { return -1; }
+            return socket.getInputStream().read();
+        }
+
+        @Override
+        public void close() throws IOException {
+            socket.getInputStream().close();
+        }
+    }
     public static void main(String[] args) throws InterruptedException {
         Thread server = new Thread(() -> {
             try {
@@ -21,12 +40,13 @@ public class SocketCloseTest {
         Thread client = new Thread(() -> {
             try {
                 Socket socket = new Socket("127.0.0.1", 6667);
-                InputStream input = socket.getInputStream();
+                InputStream input = new NonExplodingSocketInputStream(socket);
                 int b = input.read();
                 b = input.read();
                 b = input.read();
                 input.close();
                 b = input.read();
+                System.out.println(b);
             } catch (IOException e) {
                 e.printStackTrace();
             }
